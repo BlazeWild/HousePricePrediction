d936f4ef37dee0a290c47257302c73e8823329bf
Ashok Bk <123019835+BlazeWild@users.noreply.github.com>
2023-07-18 21:03:48 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+#flask, scikit-learn, pandas, pickle-mixin
+import pandas as pd
+import pickle
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+csv_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cleaned_data.csv')
+data = pd.read_csv(csv_file_path)
+model_file_path = os.path.join(os.path.dirname(__file__), 'RidgeModel.pkl')
+pipe = pickle.load(open(model_file_path, 'rb'))
+
+
+@app.route('/')
+def index():
+    
+    mainroads = sorted(data['mainroad'].unique())
+    return render_template('index.html', mainroads=mainroads)
+
+@app.route('/predict' , methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        stry = request.form.get('stories')
+        ar = request.form.get('area')
+        bds = request.form.get('bedroom')
+        bth = request.form.get('bathroom')
+        mainrd = request.form.get('mainroad')
+        furnishingstat = request.form.get('furnishingstatus')
+        
+        print(stry,ar,bds,bth,mainrd,furnishingstat)
+        input = pd.DataFrame([[stry, ar,bds,bth,mainrd,furnishingstat]],columns=['stry','ar','bds','bth','mainrd','furnishingstat'])
+        prediction = pipe.predict(input)[0]
+        
+        
+        return str(prediction)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
